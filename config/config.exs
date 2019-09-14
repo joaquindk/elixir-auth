fee41380532688fda6b23171864c7b4636374149
Joaquin Guillen Melo <joaquin@develop17.com>
2019-09-14 13:43:34 +0200

    Oauth working for the course discusss app.

---
+++ config/config.exs
@@ -0,0 +1,39 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, DiscussWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "6dif3sQ0DR/DDAXSos/83LKVLm9YH/OH9nmdoBKIYX8MMci0wPOGUAeqvK42uW5i",
+  render_errors: [view: DiscussWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, []}
+  ]
+  
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "c3108fce03fa64024951",
+  client_secret: "772011adef642dea09dbb0c27bc133f9edc3ba45"
---
+++ lib/discuss_web/router.ex
@@ -0,0 +1,41 @@
+defmodule DiscussWeb.Router do
+  use DiscussWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug DiscussWeb.Plugs.SetUser
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", DiscussWeb do
+    pipe_through :browser
+
+    # get "/", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
+
+    resources "/", TopicController
+  end
+
+  scope "/auth", DiscussWeb do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", DiscussWeb do
+  #   pipe_through :api
+  # end
+end
